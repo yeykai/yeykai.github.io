8b9bbc35f71f255c87e658ad351af6c231158c4a
yeykai <707443648@qq.com>
2019-09-12 16:58:53 +0800

    try new theme fluid

--- js/main.js
+++ js/main.js
@@ -1,96 +1,71 @@
-(function ($) {
-    $('.article img:not(".not-gallery-item")').each(function () {
-        // wrap images with link and add caption if possible
-        if ($(this).parent('a').length === 0) {
-            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
-            if (this.alt) {
-                $(this).after('<div class="has-text-centered is-size-6 has-text-grey caption">' + this.alt + '</div>');
-            }
-        }
+$(document).ready(function () {
+  var navbar = $("#navbar");
+  if (navbar.offset().top > 0) {
+    navbar.addClass("navbar-custom");
+    navbar.removeClass("navbar-dark");
+  }
+  $(window).scroll(function () {
+    if (navbar.offset().top > 0) {
+      navbar.addClass("navbar-custom");
+      navbar.removeClass("navbar-dark");
+    } else {
+      navbar.addClass("navbar-dark");
+    }
+  });
+  $('#navbar-toggler-btn').on('click', function () {
+    $('.animated-icon').toggleClass('open');
+    $('#navbar').toggleClass('navbar-col-show');
+  });
+
+  NProgress.start();
+  window.onload = function () {
+    NProgress.done();
+  };
+
+  // 向下滚动箭头的点击
+  function scrollToElement(target, offset) {
+    var scroll_offset = $(target).offset();
+    $("body,html").animate({
+      scrollTop: scroll_offset.top + (offset || 0),
+      easing: 'swing'
+    })
+  }
+
+  $(".scroll-down-bar").on("click", function () {
+    scrollToElement('#main-view', -$("#navbar").height());
+  });
+
+  // 向顶部滚动箭头
+  var topArrow = $("#scroll-top-button");
+  var posDisplay = false;
+  var scrollDisplay = false;
+  // 位置
+  var setTopArrowPos = function () {
+    var boardRight = document.getElementById('board').getClientRects()[0].right;
+    var bodyWidth = document.body.offsetWidth;
+    var right = bodyWidth - boardRight;
+    posDisplay = right >= 50;
+    topArrow.css({
+      "bottom": posDisplay && scrollDisplay ? "20px" : "-60px",
+      "right": right - 64 + "px"
     });
-
-    if (typeof (moment) === 'function') {
-        $('.article-meta time').each(function () {
-            $(this).text(moment($(this).attr('datetime')).fromNow());
-        });
-    }
-
-    $('.article > .content > table').each(function () {
-        if ($(this).width() > $(this).parent().width()) {
-            $(this).wrap('<div class="table-overflow"></div>');
-        }
+  };
+  setTopArrowPos();
+  $(window).resize(setTopArrowPos);
+  // 显示
+  var headerHeight = $("#board").offset().top;
+  $(window).scroll(function () {
+    var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop;
+    scrollDisplay = scrollHeight >= headerHeight;
+    topArrow.css({
+      "bottom": posDisplay && scrollDisplay ? "20px" : "-60px"
     });
-
-    function adjustNavbar() {
-        const navbarWidth = $('.navbar-main .navbar-start').outerWidth() + $('.navbar-main .navbar-end').outerWidth();
-        if ($(document).outerWidth() < navbarWidth) {
-            $('.navbar-main .navbar-menu').addClass('is-flex-start');
-        } else {
-            $('.navbar-main .navbar-menu').removeClass('is-flex-start');
-        }
-    }
-    adjustNavbar();
-    $(window).resize(adjustNavbar);
-
-    $('figure.highlight table').wrap('<div class="highlight-body">');
-    if (typeof (IcarusThemeSettings) !== 'undefined' &&
-        typeof (IcarusThemeSettings.article) !== 'undefined' &&
-        typeof (IcarusThemeSettings.article.highlight) !== 'undefined') {
-        if (typeof (ClipboardJS) !== 'undefined' && IcarusThemeSettings.article.highlight.clipboard) {
-            $('figure.highlight').each(function () {
-                var id = 'code-' + Date.now() + (Math.random() * 1000 | 0);
-                var button = '<a href="javascript:;" class="copy" title="Copy" data-clipboard-target="#' + id + ' .code"><i class="fas fa-copy"></i></a>';
-                $(this).attr('id', id);
-                if ($(this).find('figcaption').length) {
-                    $(this).find('figcaption').prepend(button);
-                } else {
-                    $(this).prepend('<figcaption>' + button + '</figcaption>');
-                }
-            });
-            new ClipboardJS('.highlight .copy');
-        }
-        var fold = IcarusThemeSettings.article.highlight.fold;
-        if (fold.trim()) {
-            var button = '<span class="fold">' + (fold === 'unfolded' ? '<i class="fas fa-angle-down"></i>' : '<i class="fas fa-angle-right"></i>') + '</span>';
-            $('figure.highlight').each(function () {
-                if ($(this).find('figcaption').length) {
-                    $(this).find('figcaption').prepend(button);
-                } else {
-                    $(this).prepend('<figcaption>' + button + '</figcaption>');
-                }
-            });
-
-            function toggleFold(codeBlock, isFolded) {
-                var $toggle = $(codeBlock).find('.fold i');
-                !isFolded ? $(codeBlock).removeClass('folded') : $(codeBlock).addClass('folded');
-                !isFolded ? $toggle.removeClass('fa-angle-right') : $toggle.removeClass('fa-angle-down');
-                !isFolded ? $toggle.addClass('fa-angle-down') : $toggle.addClass('fa-angle-right');
-            }
-
-            $('figure.highlight').each(function () {
-                toggleFold(this, fold === 'folded');
-            });
-            $('figure.highlight figcaption .fold').click(function () {
-                var $code = $(this).closest('figure.highlight');
-                toggleFold($code.eq(0), !$code.hasClass('folded'));
-            });
-        }
-    }
-
-    var $toc = $('#toc');
-    if ($toc.length > 0) {
-        var $mask = $('<div>');
-        $mask.attr('id', 'toc-mask');
-
-        $('body').append($mask);
-
-        function toggleToc() {
-            $toc.toggleClass('is-active');
-            $mask.toggleClass('is-active');
-        }
-
-        $toc.on('click', toggleToc);
-        $mask.on('click', toggleToc);
-        $('.navbar-main .catalogue').on('click', toggleToc);
-    }
-})(jQuery);
+  });
+  // 点击
+  topArrow.on("click", function () {
+    $("body,html").animate({
+      scrollTop: 0,
+      easing: 'swing'
+    })
+  });
+});
